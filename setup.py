e7530a294a1e968faacf7528078ab2e23bef0558
kerembay9 <kerembay9@gmail.com>
2024-11-01 01:20:43 +0300

    added stuff

--- setup.py
+++ setup.py
@@ -6,6 +6,16 @@ setup(
     author='Kerem Bayramoğlu',
     author_email='kerembay9@gmail.com',
     description='federated learning package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        # List dependencies here, for example:
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
