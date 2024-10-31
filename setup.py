cd9ca94f833765d3b4174f1b5ad515ff0d8bcae0
kerembay9 <kerembay9@gmail.com>
2024-11-01 01:14:39 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='federatedlearning',
+    version='0.0.1',
+    author='Kerem Bayramoğlu',
+    author_email='kerembay9@gmail.com',
+    description='federated learning package',
+    packages=find_packages(),
+    install_requires=[],
+)
